81627e58a63d692df4d56b16cd8c4dd4a8894ff1
ahmed aziz <ahmedsoftware93.7@gmail.com>
2022-02-02 01:10:51 +0300

    i make some screens responsive

--- game_messenger_app/lib/main.dart
+++ game_messenger_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:game_messenger_app/constants.dart';
+import 'package:game_messenger_app/screens/introduction.dart';
 import 'package:game_messenger_app/screens/more_screen/moreScreen.dart';
 import 'package:game_messenger_app/screens/verification_screen/OTP_verification_screen.dart';
 import 'package:game_messenger_app/screens/verification_screen/create_profile_screen.dart';
@@ -61,6 +62,6 @@ class AuthenticationWrapper extends StatelessWidget {
     if (theUser != null) {
       return CreateProfileAccount();
     }
-    return verification_light1();
+    return Introdution();
   }
 }
